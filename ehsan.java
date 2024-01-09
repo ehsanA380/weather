697c4a712f1d967eba135fc813de9bb7b207d987
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2024-01-09 21:53:49 +0530

    commit ehaasn .jaava

---
+++ ehsan.java
@@ -0,0 +1,22 @@
+/**
+ * ehsan
+ *
+ */
+/**
+ * Innerehsan
+ */
+ class Innerehsan {
+    public static void name() {
+        System.out.println("im inside Innereshan");
+    }
+    
+}
+public class ehsan {
+
+
+
+    public static void main(String[] args) {
+        Innerehsan.name();
+        
+    }
+}
