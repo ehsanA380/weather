eb20132511537df944dc1d206edee6f70e952176
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2024-01-09 20:32:35 +0530

    This is my first commit message in java directory

---
+++ first.java
@@ -0,0 +1,11 @@
+public class first {
+
+    
+    public static void main(String[] args) {
+        System.out.println("hellow world"+"hi");
+        //hi this is single line comment and I'd be ignored by the compiler
+        /*this 
+        is multiline 
+        comment */
+    }
+}
