0b4837432404539a6e8eef1c5f9c8e4385bd009f
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2024-01-11 12:03:46 +0530

    commit the string class implementaion

---
+++ gettingUserInput.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * gettingUserInput
+ */
+public class gettingUserInput {
+
+    public static void main(String[] args) {
+        System.out.println("Taking Input From the user: ");
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter number1");
+        // int a= sc.nextInt();
+        // System.out.println("Enter number2");
+        // int b=sc.nextInt();
+        // int sum = a+b;
+        // System.out.println("the sum of these numbers is "+sum);
+
+        // writing only one word;
+            // String str = sc.next();
+            // System.out.println(str);
+        //writing line 
+        // String line = sc.nextLine();
+        // System.out.println(line);
+
+
+        String name;
+        name = new String("Ehsan");
+        System.out.println(name);
+
+    }
+}
