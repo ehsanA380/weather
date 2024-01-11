ae36b89037f4ac6ef6c7534524dea440c9bce4ee
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2024-01-11 11:17:45 +0530

    this is the commit for adding the constructor overload

--- ehsan.java
+++ ehsan.java
@@ -18,6 +18,12 @@ class ehs {
         System.out.println("hello ");
         
     }
+    int ehs(String name){
+        System.out.println("good morning "+name);
+        return 1;
+    }
+
+    
      void playMusic() {
         System.out.println("playing music.....");
     }
@@ -29,6 +35,7 @@ public class ehsan {
 
     public static void main(String[] args) { 
         ehs music = new ehs();
+        music.ehs("ehsan");
         Innerehsan.name();
         ehs ehs1 = new ehs();
         ehs1.ehs();
