6036d3ad75155f6d6aa3ebb6186d92ccc6c5622d
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2024-01-11 11:53:25 +0530

    modification of gettingUserInput.javaj file

--- gettingUserInput.java
+++ gettingUserInput.java
@@ -8,12 +8,19 @@ public class gettingUserInput {
     public static void main(String[] args) {
         System.out.println("Taking Input From the user: ");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number1");
-        int a= sc.nextInt();
-        System.out.println("Enter number2");
-        int b=sc.nextInt();
-        int sum = a+b;
-        System.out.println("the sum of these numbers is "+sum);
+        // System.out.println("Enter number1");
+        // int a= sc.nextInt();
+        // System.out.println("Enter number2");
+        // int b=sc.nextInt();
+        // int sum = a+b;
+        // System.out.println("the sum of these numbers is "+sum);
+
+        // writing only one word;
+            // String str = sc.next();
+            // System.out.println(str);
+        //writing line 
+        String line = sc.nextLine();
+        System.out.println(line);
 
     }
 }
