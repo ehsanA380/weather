56878ca35a562c95a304cd4f350a6424384b29a0
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2024-01-11 11:26:58 +0530

    addding the file addThreeNumber.java

---
+++ addThreeNumber.java
@@ -0,0 +1,10 @@
+public class addThreeNumber {
+
+    public static void main(String[] args) {
+        int num1=6;
+        int num2=5;
+        int num3=7;
+        int sum = num1+num2+num3;
+        System.out.println("the sum of three number is "+sum);
+    }
+}
